f35e6a717eb6b5ea9bb458815c3551726afee8c5
mlepinski <mlepinski@gmail.com>
2025-06-19 13:53:03 -0400

    Modified Matt's Cropping Attempt

--- matt-crop-attempt/crop-matt-attempt.py
+++ matt-crop-attempt/crop-matt-attempt.py
@@ -50,101 +50,70 @@ def white_both_side(row, col, img):
     else:
         return False
 
+# Tries to find a black box in the image for cropping
+#  ... Starts looking at start_row, start_col
+#  ... Searches down and right from the starting position
+# Returns the subset of the image that contains the box
+def find_box(start_row, start_col, img):
+
+    num_row = image.shape[0]
+    num_col = image.shape[1]
+
+    ### Let's find the upper-left corner of a box of text
+
+    upper_left_r = start_row
+    upper_left_c = start_col
+
+    for r in range(start_row,num_row):
+        if row_both_white_black(r, image):
+            c = find_black_col(r, start_col,image)
+            if white_both_side(r, c, image):
+                print(f"Found Upper-Left -- Row: {r}, Col: {c}")
+                upper_left_r = r
+                upper_left_c = c
+                break
+
+
+    ### Now Let's find the lower right corner
+
+    new_row = upper_left_r + DELTA
+
+    found_right_side_of_box = False
+    new_col = upper_left_c + DELTA - 1
+
+    while not found_right_side_of_box:
+        new_col = find_black_col(new_row, new_col, image)
+        if white_both_side(new_row, new_col, image):
+            found_right_side_of_box = True
+
+
+    # Go down the image until you find a solid black horizontal line
+    while white_both_side(new_row, new_col, image):
+        new_row = new_row + 1
+
+    lower_right_r = new_row
+    lower_right_c = new_col
+
+
+    cropped_img = image[upper_left_r : lower_right_r, upper_left_c : lower_right_c, :]
+    print(f"Shape of Cropped Image: {cropped_img.shape}")
+    return cropped_img
+
+###################################
+## START OF MAIN PROGRAM
+###################################
+
+
 img_file = "page_02.png"
 
 image = cv2.imread(img_file)
 
-num_row = image.shape[0]
-num_col = image.shape[1]
-
 # Printing an RGB value just as a sanity check
 #print(image[1,1,:])
 
-################
-# Let's find the upper-left corner of a box of text
-
-upper_left_r = 0
-upper_left_c = 0
-
-for r in range(0,num_row):
-    if row_both_white_black(r, image):
-        c = find_black_col(r, 0,image)
-        if white_both_side(r, c, image):
-            print(f"Row: {r}, Col: {c}")
-            upper_left_r = r
-            upper_left_c = c
-            break
-
-################
-# Now Let's find the lower right corner
-
-new_row = upper_left_r + DELTA
-
-found_right_side_of_box = False
-new_col = upper_left_c + DELTA - 1
-
-while not found_right_side_of_box:
-    new_col = find_black_col(new_row, new_col, image)
-    if white_both_side(new_row, new_col, image):
-        found_right_side_of_box = True
-
-# Go down the image until you find a solid black horizontal line
-while white_both_side(new_row, new_col, image):
-    new_row = new_row + 1
-
-lower_right_r = new_row
-lower_right_c = new_col
-
-
-cropped_img = image[upper_left_r : lower_right_r, upper_left_c : lower_right_c, :]
-print(f"Shape of Cropped Image: {cropped_img.shape}")
-
+cropped_img = find_box(500,500,image)
 
 save_file_name = "test_crop.png"
 cv2.imwrite(save_file_name, cropped_img)
 print(f"  Done saving cropped image to: {save_file_name}")
 
-
-'''
-# going through each method folder to process images
-for method in methods:
-    input_folder = os.path.join(input_root, method)
-    output_folder = os.path.join(output_root, method)
-    os.makedirs(output_folder, exist_ok=True)
-
-    print(f"\nProcessing images for method: {method}")
-
-    for page_num in range(1, 8):  # pages 1 through 7
-        file_name = f"page_0{page_num}.png"
-        img_file = os.path.join(input_folder, file_name)
-        save_path = os.path.join(output_folder, file_name)
-
-        if not os.path.exists(img_file):
-            print(f"  Warning: File not found - {img_file}")
-            continue
-
-        image = cv2.imread(img_file)
-        if image is None:
-            print(f"  Error: Could not read image - {img_file}")
-            continue
-
-        gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        col_sum = np.sum(gray_img < 200, axis=0)
-
-        left_edge = 0
-        for x_pos, val in enumerate(col_sum):
-            if val > gray_img.shape[0] * 0.05:
-                left_edge = max(0, x_pos - 10)
-                break
-
-        right_edge = gray_img.shape[1] // 2
-        for x_pos in range(left_edge, gray_img.shape[1] // 2):
-            if col_sum[x_pos] < 5:
-                right_edge = x_pos + 10
-                break
-
-        cropped_img = image[:, left_edge:right_edge]
-        cv2.imwrite(save_path, cropped_img)
-        print(f"  Done saving cropped image to: {save_path}")
-
-'''
